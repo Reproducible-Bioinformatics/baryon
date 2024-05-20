ffefaa3cf40538dc47e3049baa5f3e0dfbe5ce00
Eliseo Martelli <me@eliseomartelli.it>
2024-05-20 10:40:08 +0200

    feat: add isRoxygenLine tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -4,3 +4,22 @@ import "testing"
 
 func Test_RlangDocParser(t *testing.T) {
 }
+
+func Test_IsRoxygenLine(t *testing.T) {
+	type testStruct struct {
+		Expect bool
+		Line   string
+	}
+	var tests = []testStruct{
+		{Expect: false, Line: "Not a roxygen line"},
+		{Expect: true, Line: "#' A roxygen line"},
+		{Expect: true, Line: "#'"},
+		{Expect: false, Line: "#"},
+	}
+
+	for _, entry := range tests {
+		if entry.Expect != isRoxygenLine(entry.Line) {
+			t.Errorf("%s got a wrong expectation?", entry.Line)
+		}
+	}
+}
