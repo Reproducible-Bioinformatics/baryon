1e83a91705d49b45c2977e4d3776961ce11cfad0
Eliseo Martelli <me@eliseomartelli.it>
2024-05-20 11:48:00 +0200

    feat: add first parsing function

--- parser/parser.go
+++ parser/parser.go
@@ -2,39 +2,9 @@ package parser
 
 import (
 	"baryon/tool"
-	"regexp"
-	"strings"
 )
 
 type Parser interface {
 	// Parse parses a []byte.
 	Parse([]byte) (*tool.Tool, error)
-}
-
-// roxygen implements the functions to parse R function documentation
-// and obtain a Galaxy Tool.
-type roxygen struct{}
-
-// NewRoxygen returns a New roxygen.
-func NewRoxygen() *roxygen {
-	return &roxygen{}
-}
-
-func (*roxygen) Parse(in []byte) (*tool.Tool, error) {
-	var linetoprocess []string = []string{}
-	for _, line := range strings.Split(string(in), "/n") {
-		if ok, submatches := isRoxygenLine(line); ok {
-			linetoprocess = append(linetoprocess, submatches...)
-		}
-	}
-	return &tool.Tool{}, nil
-}
-
-// roxygenLineRegex, matches a roxygenline.
-var roxygenLineRegex = regexp.MustCompile("^#' ?(.*)")
-
-// Returns true if a line is a roxygen line, false otherwise.
-func isRoxygenLine(line string) (bool, []string) {
-	submatches := roxygenLineRegex.FindStringSubmatch(line)
-	return len(submatches) > 0, submatches
 }
---
+++ parser/parser_roxygen.go
@@ -0,0 +1,50 @@
+package parser
+
+import (
+	"baryon/tool"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+// roxygen implements the functions to parse R function documentation
+// and obtain a Galaxy Tool.
+type roxygen struct{}
+
+// NewRoxygen returns a New roxygen.
+func NewRoxygen() *roxygen {
+	return &roxygen{}
+}
+
+func (*roxygen) Parse(in []byte) (*tool.Tool, error) {
+	var outtool tool.Tool
+	comment := obtainComment(in)
+	if len(comment) == 0 {
+		return nil, fmt.Errorf("Cannot parse roxygen comment.")
+	}
+	commentLines := strings.Split(comment, "\n")
+	outtool.Description = tool.Description(commentLines[0])
+	return &outtool, nil
+}
+
+// Obtains the roxygen comment form the input "in".
+func obtainComment(in []byte) string {
+	var commentLines string
+	for _, line := range strings.Split(string(in), "\n") {
+		if ok, submatches := isRoxygenLine(line); ok {
+			for _, s := range submatches[1:] {
+				commentLines += s + "\n"
+			}
+		}
+	}
+	return commentLines
+}
+
+// roxygenLineRegex, matches a roxygenline.
+var roxygenLineRegex = regexp.MustCompile("^#' ?(.*)")
+
+// Returns true if a line is a roxygen line, false otherwise.
+func isRoxygenLine(line string) (bool, []string) {
+	submatches := roxygenLineRegex.FindStringSubmatch(line)
+	return len(submatches) > 0, submatches
+}
