9c3dbaca6a87d32bda33ae36b35911408461fff1
Eliseo Martelli <me@eliseomartelli.it>
2024-05-20 10:58:11 +0200

    feat: return submatches from isRoxygenline

--- parser/parser.go
+++ parser/parser.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"baryon/tool"
 	"regexp"
+	"strings"
 )
 
 type Parser interface {
@@ -20,13 +21,20 @@ func NewRoxygen() *roxygen {
 }
 
 func (*roxygen) Parse(in []byte) (*tool.Tool, error) {
+	var linetoprocess []string = []string{}
+	for _, line := range strings.Split(string(in), "/n") {
+		if ok, submatches := isRoxygenLine(line); ok {
+			linetoprocess = append(linetoprocess, submatches...)
+		}
+	}
 	return &tool.Tool{}, nil
 }
 
 // roxygenLineRegex, matches a roxygenline.
-var roxygenLineRegex = regexp.MustCompile("^#' ?.*")
+var roxygenLineRegex = regexp.MustCompile("^#' ?(.*)")
 
 // Returns true if a line is a roxygen line, false otherwise.
-func isRoxygenLine(line string) bool {
-	return roxygenLineRegex.MatchString(line)
+func isRoxygenLine(line string) (bool, []string) {
+	submatches := roxygenLineRegex.FindStringSubmatch(line)
+	return len(submatches) > 0, submatches
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,24 +1,29 @@
 package parser
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_RlangDocParser(t *testing.T) {
 }
 
 func Test_IsRoxygenLine(t *testing.T) {
 	type testStruct struct {
-		Expect bool
-		Line   string
+		Expect     bool
+		Line       string
+		Submatches []string
 	}
 	var tests = []testStruct{
-		{Expect: false, Line: "Not a roxygen line"},
-		{Expect: true, Line: "#' A roxygen line"},
-		{Expect: true, Line: "#'"},
-		{Expect: false, Line: "#"},
+		{Expect: false, Line: "Not a roxygen line", Submatches: []string{}},
+		{Expect: true, Line: "#' A roxygen line", Submatches: []string{"A roxygen line"}},
+		{Expect: true, Line: "#'", Submatches: []string{}},
+		{Expect: false, Line: "#", Submatches: []string{}},
 	}
 
 	for _, entry := range tests {
-		if entry.Expect != isRoxygenLine(entry.Line) {
+		ok, subs := isRoxygenLine(entry.Line)
+		if ok != entry.Expect && !reflect.DeepEqual(entry.Submatches, subs) {
 			t.Errorf("%s got a wrong expectation?", entry.Line)
 		}
 	}
