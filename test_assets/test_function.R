e544493c559d7c006c35aae6dc8feac858c79bf6
Eliseo Martelli <me@eliseomartelli.it>
2024-05-29 11:47:53 +0200

    feat: add baryon annotation

--- test_assets/test_function.R
+++ test_assets/test_function.R
@@ -1,6 +1,7 @@
 #' @title name
 #' @description The present function create pseudo-bulk matrix from clustering.output file. The output are three files: _bulklog2, which is not normalized, _bulkColumn, which is z-scoere calculated over each column, _bulkRow, which is z-score calculated over each row
 #' @param group a character string. Two options: sudo or docker, depending to which group the user belongs
+#' $B{!;type(string);options(sudo,docker);}
 #' @param scratch.folderDOCKER a character string indicating the path of the scratch folder inside the docker
 #' @param scratch.folderHOST a character string indicating the path of the scratch folder inside the host. If not running from docker, this is the character string that indicates the path of the scratch.folder
 #' @param file a character string indicating the path of the file, with file name and extension included
