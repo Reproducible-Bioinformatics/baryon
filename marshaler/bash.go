cc935bf048f5ecaa3e231b2fc214c00326270804
Eliseo Martelli <me@eliseomartelli.it>
2024-09-18 10:07:34 +0200

    feat(marshaler/bash): marshal description

--- marshaler/bash.go
+++ marshaler/bash.go
@@ -2,6 +2,8 @@ package marshaler
 
 import (
 	"baryon/tool"
+	"fmt"
+	"strings"
 )
 
 // Ensure bashMarshaler implements the Marshaler interface at compile-time.
@@ -12,5 +14,22 @@ type BashMarshaler struct{}
 // Marshal implements Marshaler.
 func (b BashMarshaler) Marshal(tool *tool.Tool) ([]byte, error) {
 	buffer := []byte("#!/bin/bash\n")
+
+	if out, err := b.marshalDescription(tool.Description); err != nil {
+		return nil, fmt.Errorf("[bashMarshaler.Marshal]: %v", err)
+	} else {
+		buffer = append(buffer, out...)
+	}
+
+	return buffer, nil
+}
+func (b BashMarshaler) marshalDescription(description string) ([]byte, error) {
+	buffer := []byte("\n")
+	lines := strings.Split(description, "\n")
+	for _, l := range lines {
+		buffer = append(buffer, fmt.Sprintf("# %s\n", l)...)
+	}
+	return buffer, nil
+}
 	return buffer, nil
 }
